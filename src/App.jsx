270e0a326fbf380c70a9c5c5b1194427f613afc0
Evandro Lugli <evandro.lugli@gmail.com>
2023-11-11 01:42:10 +1100

    add sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Post } from '../src/Post';
 import { Header } from './components/Header';
 
+import styles from './App.module.css';
+
 import './global.css'
+import { Sidebar } from './Sidebar';
+
 
 export function App() {
   return (
     <div>
       <Header />
-
-      <Post 
-        author="Evandro Lugli"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consequuntur amet adipisci alias ducimus aut? Alias odio quisquam quasi beatae atque ipsam harum eos, error debitis sapiente, incidunt amet tenetur?"
-      />
-      <Post 
-        author="Felipe"
-        content="New post very very nice"
-      />
+      <div className={styles.wrapper} >
+        <Sidebar />
+        
+        <main>
+        <Post 
+          author="Evandro Lugli"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit consequuntur amet adipisci alias ducimus aut? Alias odio quisquam quasi beatae atque ipsam harum eos, error debitis sapiente, incidunt amet tenetur?"
+        />
+        <Post 
+          author="Felipe"
+          content="New post very very nice"
+        />
+        </main>
+      </div>
     </div>
 )}
