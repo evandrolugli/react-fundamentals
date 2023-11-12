6fa59cbcfd96a094bde89528771b59de6723ab8c
Evandro Lugli <evandro.lugli@gmail.com>
2023-11-12 22:25:20 +1100

    avatar and responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react'
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return(
@@ -10,10 +11,8 @@ export function Sidebar(){
                 src="https://images.unsplash.com/photo-1605379399642-870262d3d051?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
             <div className={styles.profile}>
-                <img
-                    className={styles.avatar}
-                    src="https://avatars.githubusercontent.com/u/78626020?v=4"
-                />
+                {/* <Avatar src="https://github.com/evandrolugli.png" /> */}
+                <Avatar src="https://github.com/nico9506.png" />
 
                 <strong>Evandro Lugli</strong>
                 <span>Web Developer</span>
